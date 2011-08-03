46920431170259559a299f569c58f8d9aff5e38a
Shaun Sephton <shaunsephton@gmail.com>
2011-08-03 09:30:02 +0200

    install deps

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='autoload',
+    version='0.0.1',
+    description='Simple HTTPerf loadtest runner with pretty output.',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/autoload',
+    packages = find_packages(),
+    install_requires = [
+        'gruffy',
+        'reportlab',
+    ],
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
