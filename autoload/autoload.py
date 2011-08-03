ea46c3e4f95cb80cf88aae95df04593742aac09e
Shaun Sephton <shaunsephton@gmail.com>
2011-08-03 12:41:43 +0200

    use optparse for now, python 2.6 compat

--- autoload/autoload.py
+++ autoload/autoload.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
 
-import argparse
 import ConfigParser
 
 from grapher import Grapher
 from reporter import Reporter
 from tester import Tester
     
-parser = argparse.ArgumentParser(description='Run HTTPerf loadtests and output PDF report.')
-parser.add_argument('config', type=str, nargs=1, help='configuration file detailing tests to be executed')
+#import argparse
+#parser = argparse.ArgumentParser(description='Run HTTPerf loadtests and output PDF report.')
+#parser.add_argument('config', type=str, nargs=1, help='configuration file detailing tests to be executed')
+
+from optparse import OptionParser
+import sys
+parser = OptionParser()
+parser.add_option("-c", "--config", dest="config",
+                  help="configuration file detailing tests to be executed", metavar="FILE")
+
+options, args = parser.parse_args()
+if not options.config:
+    print "Undefined config. You have to specify a test config file, see autoload -h"
+    sys.exit(1)
 
 def load_tests():
-    conf_file = vars(parser.parse_args())['config'][0]
+    #conf_file = vars(parser.parse_args())['config'][0]
+    conf_file = options.config
     config = ConfigParser.RawConfigParser()
     config.read(conf_file)
 
